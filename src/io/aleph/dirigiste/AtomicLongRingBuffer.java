c7c9c09b8978849bf24ab23a29c852a696b60100
Arnaud Geiser <arnaud.geiser@exoscale.ch>
2022-08-25 21:36:19 +0200

    fixup! fixup! Introduce a AtomicLongRingBuffer

--- src/io/aleph/dirigiste/AtomicLongRingBuffer.java
+++ src/io/aleph/dirigiste/AtomicLongRingBuffer.java
@@ -1,27 +1,31 @@
 package io.aleph.dirigiste;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicLongArray;
 
 public class AtomicLongRingBuffer {
 
-    volatile long[] _values;
+    private final AtomicLongArray _values;
     private final int _size;
     private volatile int _count;
     private volatile int _offset;
 
     public AtomicLongRingBuffer(final int size) {
-        _values = new long[size];
+        _values = new AtomicLongArray(size);
         _size = size;
     }
 
     public synchronized void add(final long value) {
-        _values[_offset++] = value;
+        _values.set(_offset++, value);
         _count = Math.min(_count + 1, _size);
         _offset %= _size;
     }
 
     public long[] toSortedArray() {
-        long[] vals = Arrays.copyOf(_values, _count);
+        long[] vals = new long[_count];
+        for (int i = 0; i < _count; i++) {
+            vals[i] = _values.get(i);
+        }
         Arrays.sort(vals);
         return vals;
     }
